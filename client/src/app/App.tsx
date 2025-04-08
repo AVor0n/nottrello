506aa1d5f392eb6330a88097fa294a6baf7af715
Andrey Voronin <AVor0n@yandex.ru>
2025-04-09 00:24:51 +0300

    build: add uikit

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,5 +1,27 @@
+import { Button, ThemeProvider, ToasterComponent, ToasterProvider, configure } from '@gravity-ui/uikit';
+import { toaster } from '@gravity-ui/uikit/toaster-singleton';
 import './global.css';
 
-const App = () => <div>test</div>;
+configure({ lang: 'en' });
+
+const App = () => (
+  <ThemeProvider theme="light">
+    <ToasterProvider toaster={toaster}>
+      <Button
+        onClick={() => {
+          toaster.add({
+            name: 'test',
+            title: 'Hello, world!',
+            content: 'This is a toast',
+            theme: 'danger',
+          });
+        }}
+      >
+        Click me
+      </Button>
+      <ToasterComponent />
+    </ToasterProvider>
+  </ThemeProvider>
+);
 
 export default App;
