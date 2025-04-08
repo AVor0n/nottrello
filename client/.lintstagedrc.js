0247cb449924eabb045fe87ba187b3a28b674b5a
Andrey Voronin <AVor0n@yandex.ru>
2025-04-09 02:21:32 +0300

    build: fix lintstaged config

--- client/.lintstagedrc.js
+++ client/.lintstagedrc.js
@@ -1,6 +1,6 @@
 export default {
-    'src/**/*.{ts,tsx}': () => ['tsc -p tsconfig.json  --noEmit'],
-    '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
-    '*.{css}': ['stylelint --fix', 'prettier --write'],
-    '!(*.{ts,tsx,css})': ['prettier --write --ignore-unknown'],
+  'src/**/*.{ts,tsx}': () => ['tsc -p tsconfig.json  --noEmit'],
+  '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.css': ['stylelint --fix', 'prettier --write'],
+  '!(*.{ts,tsx,css})': ['prettier --write --ignore-unknown'],
 };
